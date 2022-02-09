31057d6acc6db833205fa4d50bfa5a74b1e90f58
SpooRe91 <m.bogdanov9110@gmail.com>
2022-02-10 00:06:02 +0200

    update

---
+++ test/EXAM.03.cinema.test.js
@@ -0,0 +1,83 @@
+const { expect, assert } = require('chai');
+const cinema = require('../EXAM.03.cinema');
+
+describe('showMovies', () => {
+
+    describe('Testing the input length', () => {
+
+
+        it('Should return: There are currently no movies to show.', () => {
+            let arr = [];
+            expect(cinema.showMovies(arr)).to.equal('There are currently no movies to show.');
+        });
+        it('Should return an array of available movies', () => {
+            let input = ['string1', 'string2']
+            expect(cinema.showMovies(input)).to.equal('string1, string2');
+        });
+        it('Should return an array of available movies', () => {
+            let input = ['string1', 'string2', 'string3']
+            expect(cinema.showMovies(input)).to.equal('string1, string2, string3');
+        });
+    });
+
+    describe('ticketPrice', () => {
+
+        it('should check if the current projection type IS present', () => {
+            assert.equal(cinema.ticketPrice('Premiere'), '12.00');
+        });
+        it('should check if the current projection type IS present', () => {
+            assert.equal(cinema.ticketPrice('Normal'), '7.50');
+        });
+        it('should check if the current projection type IS present', () => {
+            assert.equal(cinema.ticketPrice('Discount'), '5.50');
+        });
+        it('should check if the current projection type is NOT present', () => {
+            expect(() => cinema.ticketPrice('ani')).to.throw('Invalid projection type.');
+        });
+    });
+    describe('swapSeatsInHall', function () {
+
+        it('if valid nums', () => {
+            assert.equal(cinema.swapSeatsInHall(3, 6), ('Successful change of seats in the hall.'));
+        });
+        it('if valid nums 2', () => {
+            assert.equal(cinema.swapSeatsInHall(7, 13), ('Successful change of seats in the hall.'));
+        });
+        it('if 1st reaches 20', () => {
+            assert.equal(cinema.swapSeatsInHall(7, 20), ('Successful change of seats in the hall.'));
+        });
+        it('if second reaches 20', () => {
+            assert.equal(cinema.swapSeatsInHall(20, 5), ('Successful change of seats in the hall.'));
+        });
+        it('if both are 0', () => {
+            assert.equal(cinema.swapSeatsInHall(0, 5), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if both are 0', () => {
+            assert.equal(cinema.swapSeatsInHall(5, 0), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if 1st is not num', () => {
+            assert.equal(cinema.swapSeatsInHall({ 'peshko': 0 }, 13), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if 2nd is not num', () => {
+            assert.equal(cinema.swapSeatsInHall(13, 'film'), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if 1st is below 0', () => {
+            assert.equal(cinema.swapSeatsInHall(-7, 6), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if 2nd is below 0', () => {
+            assert.equal(cinema.swapSeatsInHall(7, -13), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if 2nd is above 20', () => {
+            assert.equal(cinema.swapSeatsInHall(7, 28), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if 1st is above 20', () => {
+            assert.equal(cinema.swapSeatsInHall(28, 5), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if both are equal', () => {
+            assert.equal(cinema.swapSeatsInHall(5, 5), ('Unsuccessful change of seats in the hall.'));
+        });
+        it('if both are equal', () => {
+            assert.equal(cinema.swapSeatsInHall(34, 56), ('Unsuccessful change of seats in the hall.'));
+        });
+    });
+});
---
+++ text.js
@@ -0,0 +1,23 @@
+
+/**
+ * •	showMovies(movieArr)-A function that accepts an array:
+o	The array includes the available movies in the cinema ([‘King Kong’, ‘The Tomorrow War’, ‘Joker’,etc.])
+o	If the length of the input array is zero, the function returns the string: "There are currently no movies to show."
+o	Otherwise, the function returns:  an array of available movies, separated by a comma and space
+o	There is no need for validation for the input, you will always be given an array
+
+•	ticketPrice(projectionType)- A function that accept string:
+o	The function checks whether the submitted projectionType is present in the schedule with the types of projections
+o	If present in the schedule, return the price according to the type
+o	Otherwise, the functin throws an error in the following format "Invalid projection type."
+o	There is no need for validation for the input
+
+•	swapSeatsInHall(firstPlace, secondPlace)- A function that accepts two numbers
+o	The function swaps the seat number in the cinema hall, when two numbers are submitted.
+o	The exchange is not successful and the function returns "Unsuccessful change of seats in the hall." :
+o	If one of the two numbers do not exist
+o	The numbers are not integers
+o	If one of the numbers is greater than the capacity of the hall – 20
+o	Seats are less or equal of 0
+
+ */
