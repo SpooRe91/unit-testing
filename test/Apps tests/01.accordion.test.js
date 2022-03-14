14ae0851797d4697d71e9322802a9c9e8a5da1f2
Martin B <m.bogdanov9110@gmail.com>
2022-03-14 09:39:23 +0200

    update for app tests

---
+++ test/Apps tests/01.accordion.test.js
@@ -0,0 +1,11 @@
+const { chromium } = require('playwright-chromium');
+const { expect } = require('chai');
+
+let browser, page;
+
+it('load static page', async () => {
+    await page.goto('http://127.0.0.1:5500/01.%Accordion/index.html');
+
+    const content = await page.textContent('.accordion .head span');
+    expect(content).to.contains('Scalable Vector Graphics');
+});
