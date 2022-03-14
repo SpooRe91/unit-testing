95a7d9ae7defd8a8edb05af6453bf19eeb37cbde
Martin B <m.bogdanov9110@gmail.com>
2022-03-14 12:11:40 +0200

    update

--- test/Apps tests/01.accordion.js
+++ test/Apps tests/01.accordion.js
@@ -3,9 +3,12 @@ const { expect } = require('chai');
 
 let browser, page;
 
-it('load static page', async () => {
-    await page.goto('http://127.0.0.1:5500/01.%Accordion/index.html');
+(async () => {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+
+    await page.goto('http://127.0.0.1:5500/01.%20Accordion/index.html');
 
     const content = await page.textContent('.accordion .head span');
     expect(content).to.contains('Scalable Vector Graphics');
-});
+})();
