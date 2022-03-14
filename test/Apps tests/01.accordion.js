6adcc31845ea8ec876955b406300e90e38790852
Martin B <m.bogdanov9110@gmail.com>
2022-03-14 13:07:54 +0200

    update

--- test/Apps tests/01.accordion.js
+++ test/Apps tests/01.accordion.js
@@ -2,13 +2,14 @@ const { chromium } = require('playwright-chromium');
 const { expect } = require('chai');
 
 let browser, page;
+describe('it should work', function () {
+    it('Should work', async () => {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
 
-(async () => {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/01.%20Accordion/index.html');
 
-    await page.goto('http://127.0.0.1:5500/01.%20Accordion/index.html');
-
-    const content = await page.textContent('.accordion .head span');
-    expect(content).to.contains('Scalable Vector Graphics');
-})();
+        const content = await page.textContent('.accordion .head span');
+        expect(content).to.contains('Scalable Vector Graphics');
+    });
+})
